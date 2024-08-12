575bfb5d02a4d5ddc21fa5fc7db90fb4ed9471ef
KEREKES-jpg <178190057+KEREKES-jpg@users.noreply.github.com>
2024-08-12 19:49:00 +0200

    Add files via upload

---
+++ Plugin1v1/Commands/MatchCancelCommand.cs
@@ -0,0 +1,46 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using RemoteAdmin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin1v1.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public sealed class MatchCancelCommand : ICommand
+    {
+        public string Command { get; } = "cancel";
+        public string[] Aliases { get; }
+        public string Description { get; } = "Cancels current match or leaves party if in one";
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            var player = Player.Get(((PlayerCommandSender)sender).ReferenceHub);
+            if (player.FightManager().IsFighting)
+            {
+                if (player.FightManager().Opponent != null) player.FightManager().Opponent.FightManager().ExitMatch(MatchCancelReason.Cancelled);
+                player.FightManager().ExitMatch(MatchCancelReason.Cancelled);
+                response = "Cancelling match";
+                return true;
+            }
+            else
+            {
+                if (player.FightManager().Opponent != null)
+                {
+                    player.FightManager().Opponent.FightManager().CancelParty(true);
+                    player.FightManager().CancelParty(true);
+                    response = "Leaving party";
+                    return true;
+                }
+                else
+                {
+                    response = "You are not in match";
+                    return false;
+                }
+
+            }
+        }
+    }
+}
---
+++ Plugin1v1/Commands/MatchSelectKitCommand.cs
@@ -0,0 +1,55 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using RemoteAdmin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin1v1.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public sealed class MatchSelectKitCommand : ICommand
+    {
+        public string Command { get; } = "select";
+        public string[] Aliases { get; }
+        public string Description { get; } = "Selects kit";
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (arguments.Count != 1)
+            {
+                response = "Incorrect format, please use: .select [kit name]";
+                return false;
+            }
+            var player = Player.Get(((PlayerCommandSender)sender).ReferenceHub);
+            string desiredKit = arguments.At(0);
+            if (player.FightManager().IsFighting)
+            {
+                response = "Can't select kit while fighting!";
+                return false;
+            }
+
+            if (Plugin.Instance.Config.Loudouts.ContainsKey(desiredKit))
+            {
+                player.FightManager().selectedKit = desiredKit;
+                response = "Kit selected";
+                return true;
+            }
+            else
+            {
+                response = "Kit does not exist please select one of the following kits:";
+                foreach (var n in Plugin.Instance.Config.Loudouts)
+                {
+                    response += $"\n {n.Key}:";
+                    foreach (var m in n.Value)
+                    {
+                        response += $"\n -{m}";
+                    }
+                }
+                return false;
+            }        
+
+        }
+    }
+}
